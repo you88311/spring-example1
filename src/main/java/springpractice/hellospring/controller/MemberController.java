197bc3f56027ddde0fc1df08bb9550169e63930a
you88311 <you88311@naver.com>
2022-05-14 14:01:57 +0900

    no message

--- src/main/java/springpractice/hellospring/controller/MemberController.java
+++ src/main/java/springpractice/hellospring/controller/MemberController.java
@@ -2,6 +2,9 @@ package springpractice.hellospring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import springpractice.hellospring.domain.Member;
 import springpractice.hellospring.service.MemberService;
 
 @Controller
@@ -12,5 +15,20 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
---
+++ src/main/java/springpractice/hellospring/controller/MemberForm.java
@@ -0,0 +1,14 @@
+package springpractice.hellospring.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(){
+        this.name = name;
+    }
+
+
+}
