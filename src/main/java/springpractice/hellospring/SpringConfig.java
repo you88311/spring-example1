556d2b3e7ca10b032469031aab9a84900ae5b0b2
you88311 <you88311@naver.com>
2022-06-05 18:00:07 +0900

    no message

--- src/main/java/springpractice/hellospring/SpringConfig.java
+++ src/main/java/springpractice/hellospring/SpringConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springpractice.hellospring.repository.MemberRepository;
 import springpractice.hellospring.service.MemberService;
-
 // 자바 코드로 직접 스프링 빈 등록하는 방법
 @Configuration
 public class SpringConfig {
